9ea61e3f065bf972c75ae401de0a7c35dcb9c088
Martin <mi_kalonkin@mail.bg>
2021-06-03 08:56:37 +0300

    register connected with firebase and updated layouts

--- TennisQuizGame/app/src/main/java/com/example/tennisquizgame/RegisterActivity.java
+++ TennisQuizGame/app/src/main/java/com/example/tennisquizgame/RegisterActivity.java
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,17 +33,21 @@ public class RegisterActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
 
+    FirebaseFirestore fStore;
+    String userEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        emailField = findViewById(R.id.userNameInput);
+        emailField = findViewById(R.id.emailInput);
         passwordField = findViewById(R.id.passwordInput);
         registerBtn = (Button) findViewById(R.id.registerBtn);
         signInBtn = (Button) findViewById(R.id.loginBtn);
 
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         registerBtn.setOnClickListener(view -> {
             registerUser();
@@ -62,6 +74,16 @@ public class RegisterActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
                         Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        userEmail = mAuth.getCurrentUser().getEmail();
+                        DocumentReference docReference = fStore.collection("users").document(userEmail);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("Email", email);
+                        docReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("Success", "user profile is created for" + userEmail);
+                            }
+                        });
                         startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                     }
                     else{
